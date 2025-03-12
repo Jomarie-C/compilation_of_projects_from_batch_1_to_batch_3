276e835a4c717f022bd492839c3cc4ec58cbee4a
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-12 15:25:21 +0800

    Count odd numbers

--- Batch 1/count_odd_numbers_entered.py
+++ Batch 1/count_odd_numbers_entered.py
@@ -1,6 +1,10 @@
-# Pseudocode
-    # Enter ten(10) numbers
-    # Check if ten(10) numbers were entered
-        # Check if numbers are odd
-            # If a number entered was odd, count 1
-            # Print the odd count from the given numbers
+print("Enter ten(10) numbers:")
+
+odd_count = 0
+
+for i in range(1, 11):
+    num = int(input(f"Enter a number ({i} of 10): "))
+    if num % 2 != 0:
+        odd_count += 1
+
+print("The value of odd numbers in the given set is", + odd_count)
