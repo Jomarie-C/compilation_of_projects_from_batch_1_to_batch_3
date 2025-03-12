99d1e8ee2c413fdba2351c955dd9c3967eb733d2
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-12 12:31:57 +0800

    Pseudocode

---
+++ print_the_sum_of_ten_numbers.py
@@ -0,0 +1,5 @@
+# Pseudocode
+    # Enter ten(10) numbers
+    # Check if ten(10) numbers are entered
+        # Create an operation for the given numbers(Addition)
+        # Print the sum of the numbers entered
