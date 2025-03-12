b0dfac149e97f97ee56efd21e12661c43c4723db
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-12 10:21:06 +0800

    Print the bigger number between two numbers

--- print_the_bigger_number.py
+++ print_the_bigger_number.py
@@ -2,8 +2,8 @@ num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 
 if num1 > num2:
-    print(num1)
+    print(num1 , "is bigger than", + num2)
 elif num2 > num1:
-    print(num2)
+    print(num2 , "is bigger than", + num1)
 elif num1 == num2:
     print("Both numbers are equal.")
