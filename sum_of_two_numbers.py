1e78ba330c9dd3a64eb5016cdfa1e2b5f030877a
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-12 10:26:13 +0800

    Sum of two numbers

--- sum_of_two_numbers.py
+++ sum_of_two_numbers.py
@@ -2,4 +2,4 @@ num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 
 sum_of_two_nums = num1 + num2
-print(sum_of_two_nums)
+print("The sum of two numbers is", + sum_of_two_nums)
