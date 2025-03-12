2d0988e13ca76bcf4bcc8b011357bc858b982ad5
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-12 13:37:40 +0800

    Pseudocode

---
+++ count_odd_numbers_entered.py
@@ -0,0 +1,6 @@
+# Pseudocode
+    # Enter ten(10) numbers
+    # Check if ten(10) numbers were entered
+        # Check if numbers are odd
+            # If a number entered was odd, count 1
+            # Print the odd count from the given numbers
