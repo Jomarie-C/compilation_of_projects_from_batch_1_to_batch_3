8f7ad204dd5ee1b241391d3f460bd82e8de75346
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-12 10:24:58 +0800

    Quotient of two numbers with decimal

--- quotient_of_two_nums_with_decimal.py
+++ quotient_of_two_nums_with_decimal.py
@@ -2,4 +2,4 @@ num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 
 quotient_of_two_nums = num1 / num2
-print(quotient_of_two_nums)
+print("The quotient of two numbers is", + quotient_of_two_nums)
