977c9ed27b5edb4902038b56b5957f0ffb08b8c7
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-13 22:07:19 +0800

    Print the smaller number

--- Batch 2/print_the_smaller_number.py
+++ Batch 2/print_the_smaller_number.py
@@ -1,6 +1,9 @@
-# Pseudocode
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
 
-    # Enter two(2) numbers 
-    # Check if num1 is smaller than num2
-        # If true print num1
-        # If false print num2
+if num1 < num2:
+    print(num1 , "is smaller than", + num2)
+elif num2 < num1:
+    print(num2 , "is smaller than", + num1)
+elif num1 == num2:
+    print("Both numbers are equal.")
