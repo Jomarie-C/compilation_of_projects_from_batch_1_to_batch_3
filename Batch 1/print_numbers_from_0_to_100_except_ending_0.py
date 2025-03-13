36398ed0db17f1a79764c3b223cd9b4425488b1c
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-13 21:13:19 +0800

    Print numbers from 0 to 100 except ending with 0

--- Batch 1/print_numbers_from_0_to_100_except_ending_0.py
+++ Batch 1/print_numbers_from_0_to_100_except_ending_0.py
@@ -1,5 +1,3 @@
-# Pseudocode
-    # Assign the range while using a for loop
-        # Check if the value of the range will meet the expected result
-        # If a number is not ending with 0
-            # Print number
+for num in range(101):
+    if num % 10 != 0:
+        print(num)
