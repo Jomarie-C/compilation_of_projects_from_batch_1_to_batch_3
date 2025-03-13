718653a748c23a9141daf83930aee3b471e0ccd4
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-13 20:56:25 +0800

    Pseudocode

---
+++ Batch 1/print_even_numbers_from_0_to_100.py
@@ -0,0 +1,4 @@
+# Pseudocode
+    # Assign the range while using for loop
+        # Check if the start-stop-step will display the expected result
+        # Print even numbers starting from 0 to 100
