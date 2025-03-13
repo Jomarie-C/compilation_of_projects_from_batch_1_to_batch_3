7d748e4188844b824f8374abe776ed58bb6e4ca8
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-13 21:00:38 +0800

    Printing even numbers from 0 to 10

--- Batch 1/print_even_numbers_from_0_to_100.py
+++ Batch 1/print_even_numbers_from_0_to_100.py
@@ -1,4 +1,2 @@
-# Pseudocode
-    # Assign the range while using for loop
-        # Check if the start-stop-step will display the expected result
-        # Print even numbers starting from 0 to 100
+for num in range(0, 101, 2):
+    print(num)
