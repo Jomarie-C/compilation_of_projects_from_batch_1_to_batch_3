e1bbe28190c1fd6d822e8820a510e6fce877a75a
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-13 21:09:07 +0800

    Pseudocode

---
+++ Batch 1/print_numbers_from_0_to_100_except_ending_0.py
@@ -0,0 +1,5 @@
+# Pseudocode
+    # Assign the range while using a for loop
+        # Check if the value of the range will meet the expected result
+        # If a number is not ending with 0
+            # Print number
