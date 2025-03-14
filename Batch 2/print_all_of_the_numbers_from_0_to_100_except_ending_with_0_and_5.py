df9bbcebb28fc90c2fff429d3ab6c84fab43868d
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-14 14:17:24 +0800

    Pseudocode

---
+++ Batch 2/print_all_of_the_numbers_from_0_to_100_except_ending_with_0_and_5.py
@@ -0,0 +1,5 @@
+# Pseudocode
+    # Initialize the value of num as one
+    # Create a loop that will meet the conditions and result in the expected outcome
+        # If the number does not end with 0 and 5
+            # Print num
