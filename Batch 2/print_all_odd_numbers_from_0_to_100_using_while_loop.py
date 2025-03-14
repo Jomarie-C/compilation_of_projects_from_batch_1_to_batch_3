a4e1a6d126b9b7fcd49609f420f5da053c1a8b4a
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-14 11:17:54 +0800

    Pseudocode

---
+++ Batch 2/print_all_odd_numbers_from_0_to_100_using_while_loop.py
@@ -0,0 +1,5 @@
+# Pseudocode
+    # Initialize num to the first odd number
+    # Continue the loop until num exceeds 100
+        # Print the current value of num
+        # Increment num by 2 to get the next odd number
