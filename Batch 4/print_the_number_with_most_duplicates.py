2076aef966fc2526d088f7c32004c66f95c8a7a5
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-14 21:52:26 +0800

    Assign numbers as an empty list

--- Batch 4/print_the_number_with_most_duplicates.py
+++ Batch 4/print_the_number_with_most_duplicates.py
@@ -1,5 +1,1 @@
-# Pseudocode
-    # Set numbers as an empty list
-    # Create a loop that continuously asks the user to enter numbers until invalid input
-    # Find the number with the most duplicates
-    # Display the result
+numbers = []
