809f3cab40c4087d78d94e4bdc8f5f4c03a040bd
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-14 09:55:43 +0800

    Pseudocode

---
+++ Batch 2/difference_of_ten_numbers.py
@@ -0,0 +1,5 @@
+# Pseudocode
+     # Enter ten(10) numbers
+     # Check if ten(10) numbers were entered
+         # Create an operation for the given numbers(Subtraction)
+         # Print the difference of the numbers entered
