5732a60bc90223e65f6fbee657b0e948596b324d
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-14 23:15:33 +0800

    Printing the average of all given numbers

--- Batch 4/print_the_average_of_all_given_numbers.py
+++ Batch 4/print_the_average_of_all_given_numbers.py
@@ -6,3 +6,10 @@ while True:
         numbers.append(num)
     except:
         break
+
+if len(numbers) > 0:
+    total = sum(numbers)
+    average = total / len(numbers)
+    print("Average of the numbers:", average)
+else:
+    print("No numbers entered.")
