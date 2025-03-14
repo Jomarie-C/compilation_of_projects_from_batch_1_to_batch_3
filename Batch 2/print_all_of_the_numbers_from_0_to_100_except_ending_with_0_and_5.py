b66ea72c4e2d101bc645e0df37a6e79a8cd37fff
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-14 14:19:07 +0800

    Initialization of the value of num

--- Batch 2/print_all_of_the_numbers_from_0_to_100_except_ending_with_0_and_5.py
+++ Batch 2/print_all_of_the_numbers_from_0_to_100_except_ending_with_0_and_5.py
@@ -1,5 +1,1 @@
-# Pseudocode
-    # Initialize the value of num as one
-    # Create a loop that will meet the conditions and result in the expected outcome
-        # If the number does not end with 0 and 5
-            # Print num
+num = 1
