dfd92aa2ec1350a86e7fe05bf126d7eb50bead16
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-14 15:33:53 +0800

    Printing unique numbers in the given set

--- Batch 3/print_unique_numbers_from_10_entered_numbers.py
+++ Batch 3/print_unique_numbers_from_10_entered_numbers.py
@@ -3,3 +3,9 @@ numbers = []
 for i in range(1, 11):
     num = int(input(f"Enter a number({i} of 10): "))
     numbers += [num]
+
+print("\nThe unique numbers are:")
+
+for num in numbers:
+    if numbers.count(num) == 1:
+        print(num)
