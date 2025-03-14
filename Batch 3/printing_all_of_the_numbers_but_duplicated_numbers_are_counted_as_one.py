7a81c838cf5e8eda66bed9b1c6ee1ce70186e91c
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-14 15:50:45 +0800

    Printing all of the numbers but duplicated numbers are counted as one

--- Batch 3/printing_all_of_the_numbers_but_duplicated_numbers_are_counted_as_one.py
+++ Batch 3/printing_all_of_the_numbers_but_duplicated_numbers_are_counted_as_one.py
@@ -4,3 +4,8 @@ for i in range(1, 11):
     num = int(input(f"Enter a number({i} of 10): "))
     if num not in numbers:
         numbers.append(num)
+
+print("Displaying all of the numbers: ")
+
+for num in numbers:
+    print(num)
