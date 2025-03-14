62d6d99416135c3ecc70514fe6949a3efd77a9dc
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-14 21:45:26 +0800

    Printing all numbers that have duplicates

--- Batch 4/print_all_numbers_that_have_duplicates.py
+++ Batch 4/print_all_numbers_that_have_duplicates.py
@@ -3,3 +3,10 @@ numbers = []
 for i in range(1, 11):
     num = int(input(f"Enter a number({i} of 10): "))
     numbers.append(num)
+
+duplicates = set()
+for num in numbers:
+    if numbers.count(num) > 1:
+        duplicates.add(num)
+
+print("\nThe numbers that have duplicates are", duplicates)
