fe8f4b312bac4fa1144c2a390f8a22dfa500b145
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-14 17:14:11 +0800

    Printing all of the numbers entered from lowest to highest

--- Batch 3/print_lowest_to_highest.py
+++ Batch 3/print_lowest_to_highest.py
@@ -1,9 +1,17 @@
-numbers = []
+def main():
+    numbers = [] 
+    
+    while True:
+        user_input = input("Enter a number: ")
+        
+        try:
+            number = int(user_input)  
+            numbers.append(number) 
+        except:
+            break  
+    
+    numbers.sort() 
+    print("\nNumbers from lowest to highest:", numbers)
+    print("\nExiting the program.")
 
-while True:
-    user_input = (input("Enter a number: "))
-    try:
-        number = int(user_input)
-        numbers.append(number)  
-    except:
-        break
+main()
