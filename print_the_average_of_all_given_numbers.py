63ef94aa4ffc6cee662e51a04a0cbeeb6e221776
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-14 23:06:00 +0800

    Pseudocode

---
+++ print_the_average_of_all_given_numbers.py
@@ -0,0 +1,9 @@
+# Pseudocode
+    # Assign numbers as an empty list
+    # Create a loop that continuously asks user for input until invalid input
+    # Create a condition to meet the expected outcome
+        # Create an operation using functions sum() and len()
+        # If numbers entered is greater than zero
+            # Print average
+        # If numbers is less than or equal to zero
+            # Print 'No numbers entered'
