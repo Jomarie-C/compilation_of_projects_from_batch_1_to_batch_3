11ccdc585f0958f0599fa634b331bb695edc1681
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-14 11:07:50 +0800

    Count even numbers entered

--- Batch 2/count_even_numbers_entered.py
+++ Batch 2/count_even_numbers_entered.py
@@ -1,6 +1,10 @@
-# Pseudocode
-    # Enter ten(10) numbers
-    # Check if ten(10) numbers were entered
-        # Check if a number is even
-            # If a number entered was even, count 1
-            # Print the even count from the given numbers
+print("Enter ten(10) numbers:")
+ 
+even_count = 0
+ 
+for i in range(1, 11):
+    num = int(input(f"Enter a number ({i} of 10): "))
+    if num % 2 == 0:
+        even_count += 1
+ 
+print("The value of even numbers in the given set is", + even_count)
