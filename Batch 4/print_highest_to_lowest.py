fe6faea5e6b251f9b51f9fe77ecd5ae991cf083a
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-14 22:50:49 +0800

    Pseudocode

---
+++ Batch 4/print_highest_to_lowest.py
@@ -0,0 +1,5 @@
+# Pseudocode
+    # Assign numbers as an empty list
+    # Create a loop that continuously asks user for input until invalid input
+    # Use the sort function to sort numbers in ascending form and use the reverse function to convert it in descending form
+    # Print numbers from highest to lowest
