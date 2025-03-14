38bcf0f7bdf04f5fe45172ea71b8f0b50db534d3
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-14 22:31:41 +0800

    Pseudocode

---
+++ Batch 4/print_highest_number.py
@@ -0,0 +1,5 @@
+# Pseudocode
+    # Assign numbers as an empty list
+    # Create a loop that continuously asks the user for input until invalid input
+    # Create another loop to check the highest number
+    # Print the highest number given
