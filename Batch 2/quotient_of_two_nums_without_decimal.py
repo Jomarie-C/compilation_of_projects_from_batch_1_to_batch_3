f5b37baff5207a536500c3ce6a1800003de29091
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-14 09:02:46 +0800

    Quotient of two(2) numbers without decimal(Floor Division)

--- Batch 2/quotient_of_two_nums_without_decimal.py
+++ Batch 2/quotient_of_two_nums_without_decimal.py
@@ -1,3 +1,5 @@
 num1 = float(input("Enter the dividend: "))
 num2 = float(input("Enter the divisor: "))
 
+quotient_of_two_nums = num1 // num2
+print("The quotient of two numbers is", + quotient_of_two_nums)
