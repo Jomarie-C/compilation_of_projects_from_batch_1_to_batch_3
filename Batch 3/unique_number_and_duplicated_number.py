aeffd5fab76686c9232e63929357ad7de18d8e11
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-14 16:02:03 +0800

    Pseudocode

---
+++ Batch 3/unique_number_and_duplicated_number.py
@@ -0,0 +1,5 @@
+# Pseudocode
+    # Initialize numbers as an empty list
+    # Ask the user to input 10 numbers
+    # Check if the number is unique or a duplicate
+    # Stop the loop if an invalid input is entered
