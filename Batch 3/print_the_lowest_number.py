fdff9f2eceed79f6f5142f361c0c87126a16d33c
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-14 16:37:14 +0800

    Continuously ask the user to input numbers until input is invalid

--- Batch 3/print_the_lowest_number.py
+++ Batch 3/print_the_lowest_number.py
@@ -1,1 +1,11 @@
 lowest_number = float('inf')
+
+while True:
+    user_input = input("Enter a number: ")
+        
+    if user_input.isdigit():
+        number = int(user_input)
+    elif number < lowest_number:
+        lowest_number = number
+    else:
+        break
