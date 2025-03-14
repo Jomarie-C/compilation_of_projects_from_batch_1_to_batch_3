3b4ee4b19c68d29edd74de6db2c3bce61a738b17
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-14 10:28:23 +0800

    Assigning a value for range while using a for loop. Asking for the remaining numbers. Create an operation(subtraction) to execute.

--- Batch 2/difference_of_ten_numbers.py
+++ Batch 2/difference_of_ten_numbers.py
@@ -1,2 +1,7 @@
 first_number = float(input("Enter the first number: "))
-first_number = result
+result = first_number
+
+for i in range(1, 10):
+    number = float(input(f"Enter the next number({i} of 9): "))
+    result -= number
+
