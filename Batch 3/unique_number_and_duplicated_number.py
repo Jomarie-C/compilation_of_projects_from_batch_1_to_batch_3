17e34a8cb658404a62f47f5f8e59fd0f3890597d
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-14 16:14:27 +0800

    Check if the number is unique or duplicate

--- Batch 3/unique_number_and_duplicated_number.py
+++ Batch 3/unique_number_and_duplicated_number.py
@@ -2,3 +2,8 @@ numbers = []
 
 for i in range(1, 11):
         num = int(input(f"Enter a number({i} of 10): "))  
+        if numbers.count(num) == 0:
+            print("Unique")
+        else:
+            print("Duplicate")
+        numbers.append(num)
