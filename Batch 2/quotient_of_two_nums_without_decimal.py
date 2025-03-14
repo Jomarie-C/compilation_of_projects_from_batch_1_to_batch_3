4e1431257ad73390d35ee3f4061547020d9e3d20
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-14 08:59:06 +0800

    Asking the user to input two(2) numbers

--- Batch 2/quotient_of_two_nums_without_decimal.py
+++ Batch 2/quotient_of_two_nums_without_decimal.py
@@ -1,4 +1,3 @@
-# Pseudocode
-    # Enter two(2) numbers
-    # Create an operation for both numbers(Floor Division)
-        # Print the quotient of two(2) numbers
+num1 = float(input("Enter the dividend: "))
+num2 = float(input("Enter the divisor: "))
+
