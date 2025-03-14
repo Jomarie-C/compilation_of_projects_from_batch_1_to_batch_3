971fc2c05176a98256270f7eb14ec12cc196216e
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-14 16:38:41 +0800

    Printing the lowest number given

--- Batch 3/print_the_lowest_number.py
+++ Batch 3/print_the_lowest_number.py
@@ -1,11 +1,16 @@
-lowest_number = float('inf')
-
-while True:
-    user_input = input("Enter a number: ")
+def main():
+    lowest_number = float('inf')
+    
+    while True:
+        user_input = input("Enter a number: ")
         
-    if user_input.isdigit():
-        number = int(user_input)
-    elif number < lowest_number:
-        lowest_number = number
-    else:
-        break
+        if user_input.isdigit():
+            number = int(user_input)
+            if number < lowest_number:
+                lowest_number = number
+        else:
+            break
+    
+    print("The lowest number given is", lowest_number)
+
+main()
