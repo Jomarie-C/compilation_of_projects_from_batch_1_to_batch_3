84ac08b94035d9d99bb6627903a5284e36dcc977
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-14 10:17:30 +0800

    Get the first number

--- Batch 2/difference_of_ten_numbers.py
+++ Batch 2/difference_of_ten_numbers.py
@@ -1,8 +1,1 @@
-# Pseudocode
-    # Ask the user to enter the first number
-    # Initialize the first number as the result
-    # Assign a value for range while using a for loop
-    # Enter the remaining numbers(9)
-    # Check if the remaining numbers were entered
-        # Create an operation for the given numbers(Subtraction)
-        # Print the result of the first number minus all of the remaining number
+first_number = float(input("Enter the first number: "))
