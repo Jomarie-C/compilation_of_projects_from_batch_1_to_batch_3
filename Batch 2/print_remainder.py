5e566dc967fee72f1750f6fc261d349923ba945f
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-14 09:40:26 +0800

    Remainder of two numbers

--- Batch 2/print_remainder.py
+++ Batch 2/print_remainder.py
@@ -1,2 +1,5 @@
 num1 = float(input("Enter the dividend: "))
 num2 = float(input("Enter the divisor: "))
+
+remainder_of_two_nums = num1 % num2
+print("The remainder of two numbers is", + remainder_of_two_nums)
