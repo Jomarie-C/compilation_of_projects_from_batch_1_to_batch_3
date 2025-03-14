a318ef2d14cedf1de9920b2ce90a45702da471d9
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-14 16:31:02 +0800

    Pseudocode

---
+++ Batch 3/print_the_lowest_number.py
@@ -0,0 +1,6 @@
+# Pseudocode
+    # Initialize lowest number to infinity
+    # Make a loop that will continuously ask the user to input numbers
+        # Update lowest number
+        # Break the loop if the user entered an invalid character
+        # Print the lowest number
