a6927a443ffa11ab4befcdd93d611344a1db2884
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-14 22:52:42 +0800

    Assigning numbers as an empty list. Creating a loop that continuously asks user for input until invalid input

--- Batch 4/print_highest_to_lowest.py
+++ Batch 4/print_highest_to_lowest.py
@@ -1,5 +1,8 @@
-# Pseudocode
-    # Assign numbers as an empty list
-    # Create a loop that continuously asks user for input until invalid input
-    # Use the sort function to sort numbers in ascending form and use the reverse function to convert it in descending form
-    # Print numbers from highest to lowest
+numbers = []
+
+while True:
+    try:
+        num = int(input("Enter a number: "))
+        numbers.append(num)
+    except:
+        break
