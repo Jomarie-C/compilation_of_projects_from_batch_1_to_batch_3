4e370ce13bbf27ad0ec167a24f950961509b85cb
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-14 21:59:51 +0800

    Assigning values of most common number and max count. Creating a loop to check what is the most common number entered

--- Batch 4/print_the_number_with_most_duplicates.py
+++ Batch 4/print_the_number_with_most_duplicates.py
@@ -6,3 +6,12 @@ while True:
         numbers.append(num)
     except:
         break
+
+most_common = None
+max_count = 0
+
+for num in numbers:
+    count = numbers.count(num)
+    if count > max_count:
+        max_count = count
+        most_common = num
