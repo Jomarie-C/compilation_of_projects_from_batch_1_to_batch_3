fdc5c3321cdb03b50778eead0b8d6783986de60b
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-14 10:37:30 +0800

    Pseudocode

---
+++ Batch 2/count_even_numbers_entered.py
@@ -0,0 +1,6 @@
+# Pseudocode
+    # Enter ten(10) numbers
+    # Check if ten(10) numbers were entered
+        # Check if a number is even
+            # If a number entered was even, count 1
+            # Print the even count from the given numbers
