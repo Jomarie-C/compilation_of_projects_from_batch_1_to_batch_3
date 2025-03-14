38458fd46ad61a654240214d90266fedf9ef3728
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-14 16:58:42 +0800

    Pseudocode

---
+++ Batch 3/print_lowest_to_highest.py
@@ -0,0 +1,7 @@
+# Pseudocode
+    # Assign numbers as an empty list
+    # Create a loop that continuously asks the user for inputs
+        # Add every valid input to the list
+        # Once entered an invalid input
+            # Break the loop
+    # Print numbers entered from lowest to highest
