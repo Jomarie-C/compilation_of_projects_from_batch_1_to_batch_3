15cc3be16a5fc642659e26b87134232b4c979dcf
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-14 15:44:27 +0800

    Initializing numbers as an empty list

--- Batch 3/printing_all_of_the_numbers_but_duplicated_numbers_are_counted_as_one.py
+++ Batch 3/printing_all_of_the_numbers_but_duplicated_numbers_are_counted_as_one.py
@@ -1,4 +1,1 @@
-# Pseudocode
-    # Initialize numbers as an empty list
-    # Create a loop that will ask the user to input 10 numbers
-    # Create another loop to check the duplicated numbers and display all of the numbers given
+numbers = []
