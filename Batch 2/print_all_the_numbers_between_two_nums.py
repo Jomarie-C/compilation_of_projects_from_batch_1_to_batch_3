d016d05f2f0993281e84284a11a54953aca2702d
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-14 14:40:23 +0800

    Pseudocode

---
+++ Batch 2/print_all_the_numbers_between_two_nums.py
@@ -0,0 +1,7 @@
+# Pseudocode
+    # Ask the user to input two numbers
+    # Create a loop that will meet the expected outcome
+        # While num1 is less than or equal to num2
+            # Print num1 and add 1
+
+
