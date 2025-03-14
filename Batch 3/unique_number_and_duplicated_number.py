a38d39dbd3e8f02df93d4715a7dff38784248772
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-14 16:16:43 +0800

    Continue checking until user entered invalid input

--- Batch 3/unique_number_and_duplicated_number.py
+++ Batch 3/unique_number_and_duplicated_number.py
@@ -1,9 +1,14 @@
 numbers = []
 
 for i in range(1, 11):
-        num = int(input(f"Enter a number({i} of 10): "))  
+    try:
+        num = int(input(f"Enter a number({i} of 10): "))    
         if numbers.count(num) == 0:
             print("Unique")
         else:
             print("Duplicate")
         numbers.append(num)
+    
+    except ValueError:
+        print("Invalid input, stopping.")
+        break  
