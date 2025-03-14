9a4b15a69ab7676e246dc39e54a6c5d94c293cc7
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-14 15:27:52 +0800

    Initializing numbers as an empty list

--- Batch 3/print_unique_numbers_from_10_entered_numbers.py
+++ Batch 3/print_unique_numbers_from_10_entered_numbers.py
@@ -1,5 +1,1 @@
-# Pseudocode
-    # Initialize numbers as an empty list
-    # Create a loop that will ask the user to input numbers ten times
-    # Create another loop for it to check how many unique numbers are in the given set
-        # Print unique numbers
+numbers = []
